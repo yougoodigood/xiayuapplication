ca975e2eab37edf6657b81ea5ee037d2de0857c3
sunliqian <sunliqian@yovole.com>
2020-02-08 00:47:53 +0800

    高可用注册中心

--- CommonIntegration/src/main/java/com/xiayu/common/integration/controller/PortController.java
+++ CommonIntegration/src/main/java/com/xiayu/common/integration/controller/PortController.java
@@ -1,11 +1,13 @@
 package com.xiayu.common.integration.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class PortController {
 
     @Value("${xiayu.feature.mail}")
--- UserService/src/main/java/com/xiayu/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/xiayu/user/service/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.xiayu.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix //开启hystrix断路器
+@RefreshScope
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class);
