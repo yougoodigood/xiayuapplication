76646366a89de914c00ed8f41c4033bd5e6dbcf0
sunliqian <sunliqian@yovole.com>
2020-02-07 15:29:18 +0800

    ribbon+restTemplate feign hystrix zuul demo finish

---
+++ configcenter/src/main/java/com/xiayu/config/center/ConfigCenterApplication.java
@@ -0,0 +1,13 @@
+package com.xiayu.config.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class);
+    }
+}
